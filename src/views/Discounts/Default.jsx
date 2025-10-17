5a0066c3422951ff60f35fb1a6b0c824c4380c4b
hiepnt <tienhiep345@gmail.com>
2025-10-18 00:16:19 +0700

    Done trang danh sach ma giam gia

--- src/views/Discounts/Default.jsx
+++ src/views/Discounts/Default.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect } from 'react';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import { Table, Tag } from 'antd';
+import { Table, Tag, Col, Row, Input, Flex, Button, Space, Select } from 'antd';
+import { SearchOutlined, DownOutlined, ReloadOutlined, PlusOutlined } from '@ant-design/icons';
 import MainCard from 'components/MainCard';
 import SearchDiscountQuery from '../../DTO/Discounts/SearchDiscounts/SearchDiscountQuery';
+import DiscountFilter from '../../DTO/Discounts/SearchDiscounts/SearchDiscountFilter';
 import Constants from '../../Constants/Constants';
 import Utility from '../../utils/Utility';
 import LoadingModal from '../../components/LoadingModal';
+import { set } from 'react-hook-form';
 
 export default function Default() {
-    const [filter, setFilter] = React.useState(new SearchDiscountQuery());
+    const [query, setQuery] = React.useState(new SearchDiscountQuery());
+    const [filter, setFilter] = React.useState(new DiscountFilter());
     const [listDiscount, setListDiscount] = React.useState([]);
+    const [listStatus, setListStatus] = React.useState([]);
+    const [isReset, setIsReset] = React.useState(false);
 
     const columns = [
         {
             title: 'STT',
             key: 'index',
-            render: (_, __, index) => index + 1
+            render: (_, __, index) => (filter.PageIndex ?? 0) * Constants.DEFAULT_PAGE_SIZE + index + 1
         },
         {
             title: 'Mã',
@@ -60,22 +64,41 @@ export default function Default() {
             key: 'statusName',
             align: 'center',
             render: (value, record) => {
-               return record.status === Constants.Status.Active ? 
-                <Tag color="green">{value}</Tag> : <Tag color="red">{value}</Tag>
+                return record.status === Constants.Status.Active ? <Tag color="green">{value}</Tag> : <Tag color="red">{value}</Tag>;
             }
         }
     ];
 
     useEffect(() => {
-        searchData(0);
+        searchData();
+        setupDefault();
     }, []);
+
+    useEffect(() => {
+        if (filter && query && isReset) {
+            searchData(0);
+        }
+    }, [filter, query, isReset]);
+
+    const setupDefault = async () => {
+        const response = await fetch('https://localhost:44331/api/Discount/setup-default', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({})
+        });
+        const res = await response.json();
+        setListStatus(res);
+    };
 
     const searchData = async (pageIndex = 0) => {
         try {
             LoadingModal.showLoading();
-            const request = { ...filter };
+            const request = { ...query };
             request.PageIndex = pageIndex;
             request.PageSize = Constants.DEFAULT_PAGE_SIZE;
+            request.DiscountFilter = { ...filter };
             const response = await fetch('https://localhost:44331/api/Discount/search', {
                 method: 'POST',
                 headers: {
@@ -84,8 +107,8 @@ export default function Default() {
                 body: JSON.stringify(request)
             });
             const res = await response.json();
-            console.log('Search results:', res);
             setListDiscount(res.listDiscount);
+            setIsReset(false);
         } catch (error) {
             console.error('Error fetching discounts:', error);
         } finally {
@@ -93,16 +116,96 @@ export default function Default() {
         }
     };
 
+    const onReset = () => {
+        const newFilter = new DiscountFilter();
+        const newQuery = new SearchDiscountQuery();
+        setFilter(newFilter);
+        setQuery(newQuery);
+        setIsReset(!isReset);
+    };
+
     return (
         <Row>
-            <Col xl={12}>
-                <MainCard title="Danh sách mã giảm giá">
+            <Col span={24}>
+                <MainCard 
+                    title="Danh sách mã giảm giá"
+                    secondary={
+                        <Button type="primary" href="/admin/sale/discount/addnew" shape="round" icon={<PlusOutlined />}>
+                            Tạo mã giảm giá
+                        </Button>
+                    }
+                >
+                    <Row gutter={[24, 24]} className="mb-5">
+                        <Col span={6}>
+                            <Flex gap="small" align="center">
+                                <span>Mã: </span>
+                                <Input
+                                    value={filter.Code}
+                                    onChange={(e) => {
+                                        filter.Code = e.target.value;
+                                        setFilter({ ...filter });
+                                    }}
+                                />
+                            </Flex>
+                        </Col>
+                        <Col span={6}>
+                            <Flex gap="small" align="center">
+                                <span>Tên mã: </span>
+                                <Input
+                                    style={{ flex: 1 }}
+                                    value={filter.Name}
+                                    onChange={(e) => {
+                                        filter.Name = e.target.value;
+                                        setFilter({ ...filter });
+                                    }}
+                                />
+                            </Flex>
+                        </Col>
+                        <Col span={6}>
+                            <Flex gap="small" align="center">
+                                <span>Trạng thái: </span>
+                                <Select
+                                    value={filter.Status}
+                                    allowClear
+                                    style={{ flex: 1 }}
+                                    options={listStatus.map((item) => ({
+                                        label: item.value,
+                                        value: item.key
+                                    }))}
+                                    onChange={(val) => {
+                                        filter.Status = val;
+                                        setFilter({ ...filter });
+                                    }}
+                                />
+                            </Flex>
+                        </Col>
+                        <Col span={6}>
+                            <Row justify="end">
+                                <Space>
+                                    <Button type="primary" icon={<SearchOutlined />} shape="round" onClick={() => searchData()}>
+                                        Tìm kiếm
+                                    </Button>
+                                    <Button type="primary" icon={<ReloadOutlined />} shape="round" onClick={() => onReset()}>
+                                        Reset
+                                    </Button>
+                                </Space>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <h6 className="mb-3">Tổng số bản ghi: {listDiscount.length}</h6>
                     <Table
                         dataSource={listDiscount}
                         columns={columns}
                         rowKey={(record) => record.code || record.id}
-                        pagination={{ pageSize: Constants.DEFAULT_PAGE_SIZE }}
                         bordered
+                        pagination={{
+                            pageSize: Constants.DEFAULT_PAGE_SIZE,
+                            onChange: (page) => {
+                                searchData(page - 1);
+                                query.PageIndex = page - 1;
+                                setQuery({ ...query });
+                            }
+                        }}
                     />
                 </MainCard>
             </Col>
